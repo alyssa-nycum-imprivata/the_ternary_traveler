9d5a62e898e05ca20e245588bc2e9db5118d8776
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-02-13 10:47:49 -0600

    added save interest event listener

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -1,14 +1,22 @@
 const baseUrl = "http://localhost:8088";
 
 const placesExtension = "/places";
-
 const interestsExtension = "/interests";
-
 const expandPlace = "?_expand=place"
 
 const apiManager = {
     getAllInterests() {
         return fetch(baseUrl + interestsExtension + expandPlace)
+            .then(resp => resp.json())
+    },
+    addNewInterest(newInterest) {
+        return fetch(baseUrl + interestsExtension + expandPlace, {
+           method: "POST",
+           headers: {
+               "Content-Type": "application/json"
+           },
+           body: JSON.stringify(newInterest) 
+        })
             .then(resp => resp.json())
     }
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,9 @@
 import apiManager from './apiManager.js';
 import renderComponents from './renderComponents.js';
+import events from './events.js';
 
 apiManager.getAllInterests();
 
 renderComponents.renderInterests();
+
+events.addSaveButtonEventListenter();
